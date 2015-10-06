010c03e703ed00a64f688776191733b4d12f54b0
Jessica Hanrahan <jessica.hanrahan@tamu.edu>
2015-10-06 14:58:11 -0500

    Pushing new changes

--- source/matching/MatchFactory.cs
+++ source/matching/MatchFactory.cs
@@ -1,5 +1,8 @@
 using System.Collections.Generic;
+using code.enumerables;
 using code.prep.movies;
+using Machine.Specifications;
+using NHibernate.Exceptions;
 
 namespace code.matching
 {
@@ -14,21 +17,37 @@ namespace code.matching
 
     public IMatchAn<Item> equal_to(AttributeType value)
     {
-      return new AnonymousMatch<Item>(x => accessor(x).Equals(value));
+      return equal_to_any(value);
     }
 
     public IMatchAn<Item> equal_to_any(params AttributeType[] values)
     {
-        IList<IMatchAn<Item>> match_list = new List<IMatchAn<Item>>();
-        IMatchAn<Item> retVal = null;
-        foreach (AttributeType val in values)
-        {
-             match_list.Add(equal_to(val));
-        }
-            
-            //get iMatchans
-            //then use OrMatch between them to build 1 imatcha
-        return ;
+            //IMatchAn<Item> retVal = NeverMatches<Item>.instance;
+            //foreach (AttributeType val in values)
+            //{
+            //    retVal = retVal.or(equal_to(val));
+            //}
+
+            //return retVal;
+
+            return new AnonymousMatch<Item>(x => new List<AttributeType>(values).Contains(accessor(x)));
     }
+
+      public IMatchAn<Item> not_equal_to(AttributeType value)
+      {
+          return new AnonymousMatch<Item>(x => accessor(x).Equals(value));
+      } 
   }
+
+    public class NeverMatches<Item> : IMatchAn<Item>
+    {
+        public static readonly IMatchAn<Item> instance = new NeverMatches<Item>();
+        
+        NeverMatches() { }
+        
+        public bool matches(Item item)
+        {
+            return false;
+        }  
+    }
 }
