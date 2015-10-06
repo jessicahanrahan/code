b8916bdd2fa13025334b858e555a40d9cd856b30
Jessica Hanrahan <jessica.hanrahan@tamu.edu>
2015-10-06 09:02:57 -0500

    Pushing new changes

--- source/prep/movies/Movie.cs
+++ source/prep/movies/Movie.cs
@@ -1,13 +1,71 @@
 using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Web.UI.WebControls;
 
 namespace code.prep.movies
 {
-  public class Movie
-  {
-    public string title { get; set; }
-    public ProductionStudio production_studio { get; set; }
-    public Genre genre { get; set; }
-    public int rating { get; set; }
-    public DateTime date_published { get; set; }
-  }
+    public class Movie
+    {
+        public string title { get; set; }
+        public ProductionStudio production_studio { get; set; }
+        public Genre genre { get; set; }
+        public int rating { get; set; }
+        public DateTime date_published { get; set; }
+
+        public List<Movie> CreateSortList<T>(
+            IEnumerable<Movie> dataSource,
+            string fieldName, SortDirection sortDirection)
+        {
+            List<Movie> returnList = new List<Movie>();
+            returnList.AddRange(dataSource);
+            // get property from field name passed
+            PropertyInfo propInfo = typeof(T).GetProperty(fieldName);
+            Comparison<Movie> compare = delegate(Movie a, Movie b)
+            {
+                bool asc = sortDirection == SortDirection.Ascending;
+                object valueA = asc ? propInfo.GetValue(a, null) : propInfo.GetValue(b, null);
+                object valueB = asc ? propInfo.GetValue(b, null) : propInfo.GetValue(a, null);
+                //comparing the items
+                return valueA is IComparable ? ((IComparable) valueA).CompareTo(valueB) : 0;
+            };
+            returnList.Sort(compare);
+            return returnList;
+        }
+
+        public List<Movie> CreateSortList<T>(
+            IEnumerable<Movie> dataSource,
+            string fieldName1, SortDirection sortDirection1,
+            string fieldName2, SortDirection sortDirection2)
+        {
+            List<Movie> returnList = new List<Movie>();
+            returnList.AddRange(dataSource);
+            // get property from field name passed
+            PropertyInfo propInfo1 = typeof(T).GetProperty(fieldName1);
+            PropertyInfo propInfo2 = typeof(T).GetProperty(fieldName2);
+            Comparison<Movie> compare = delegate(Movie a, Movie b)
+            {
+                bool asc = sortDirection1 == SortDirection.Ascending;
+                object valueA = asc ? propInfo1.GetValue(a, null) : propInfo1.GetValue(b, null);
+                object valueB = asc ? propInfo1.GetValue(b, null) : propInfo1.GetValue(a, null);
+
+                if (valueA is IComparable)
+                {
+                    var comparison = ((IComparable) valueA).CompareTo(valueB);
+                    if (comparison == 0)
+                    {
+                        bool asc2 = sortDirection2 == SortDirection.Ascending;
+                        object valueA2 = asc2 ? propInfo2.GetValue(a, null) : propInfo2.GetValue(b, null);
+                        object valueB2 = asc2 ? propInfo2.GetValue(b, null) : propInfo2.GetValue(a, null);
+
+                        return valueA2 is IComparable ? ((IComparable) valueA2).CompareTo(valueB2) : 0;
+                    }
+                }
+                //comparing the items
+                return valueA is IComparable ? ((IComparable) valueA).CompareTo(valueB) : 0;
+            };
+            returnList.Sort(compare);
+            return returnList;
+        }
+    }
 }
--- source/prep/movies/MovieLibrary.cs
+++ source/prep/movies/MovieLibrary.cs
@@ -1,184 +1,171 @@
-using System;
 using System.Collections.Generic;
 using System.Web.UI.WebControls;
 
 namespace code.prep.movies
 {
-  public class MovieLibrary
-  {
-    readonly IList<Movie> movies;
-
-    public MovieLibrary(IList<Movie> list_of_movies)
+    public class MovieLibrary
     {
-      this.movies = list_of_movies;
-    }
+        readonly IList<Movie> movies;
 
-    public IEnumerable<Movie> all_movies()
-    {
-        foreach (var movie in movies)
+        public MovieLibrary(IList<Movie> list_of_movies)
         {
+            this.movies = list_of_movies;
+        }
+
+        public IEnumerable<Movie> all_movies()
+        {
+            foreach (var movie in movies)
+            {
                 yield return movie;
-           }
-    }
-
-    public void add(Movie movie)
-    {
-        if (!movies.Contains(movie)) 
-        {
-            foreach (var m in movies)
-            {
-                if (movie.title == m.title)
-                    return;
             }
-
-            movies.Add(movie);
         }
-    }
 
-    public IEnumerable<Movie> all_movies_published_by_pixar()
-    {
-      var action_movies = new List<Movie>();
-      foreach(var m in all_movies())
-      {
-        if (m.production_studio == ProductionStudio.Pixar)
+        public void add(Movie movie)
         {
-          action_movies.Add(m);
-        }
-      }
-      return action_movies;
-    }
-
-    public IEnumerable<Movie> all_movies_published_by_pixar_or_disney()
-    {
-      var action_movies = new List<Movie>();
-      foreach(var m in all_movies())
-      {
-        if (m.production_studio == ProductionStudio.Pixar
-            || m.production_studio == ProductionStudio.Disney)
-        {
-          action_movies.Add(m);
-        }
-      }
-      return action_movies;
-    }
-
-    public IEnumerable<Movie> all_movies_not_published_by_pixar()
-    {
-      var action_movies = new List<Movie>();
-      foreach(var m in all_movies())
-      {
-        if (m.production_studio != ProductionStudio.Pixar)
-        {
-          action_movies.Add(m);
-        }
-      }
-      return action_movies;
-    }
-
-    public IEnumerable<Movie> all_movies_published_after(int year)
-    {
-      var action_movies = new List<Movie>();
-      foreach(var m in all_movies())
-      {
-        if (m.date_published.Year > year)
-        {
-          action_movies.Add(m);
-        }
-      }
-      return action_movies;
-    }
-
-    public IEnumerable<Movie> all_movies_published_between_years(int startingYear, int endingYear)
-    {
-      var action_movies = new List<Movie>();
-      foreach(var m in all_movies())
-      {
-        if (m.date_published.Year >= startingYear 
-            && m.date_published.Year <= endingYear)
-        {
-          action_movies.Add(m);
-        }
-      }
-      return action_movies;
-    }
-
-    public IEnumerable<Movie> all_kid_movies()
-    {
-      var action_movies = new List<Movie>();
-      foreach(var m in all_movies())
-      {
-        if (m.genre == Genre.kids)
-        {
-          action_movies.Add(m);
-        }
-      }
-      return action_movies;
-    }
-
-    public IEnumerable<Movie> all_action_movies()
-    {
-      var action_movies = new List<Movie>();
-      foreach(var m in all_movies())
-      {
-        if (m.genre == Genre.action)
-        {
-          action_movies.Add(m);
-        }
-      }
-      return action_movies;
-    }
-
-        private List<Movie> CreateSortList<T>(
-                        IEnumerable<Movie> dataSource,
-                        string fieldName, SortDirection sortDirection)
-        {
-            List<Movie> returnList = new List<Movie>();
-            returnList.AddRange(dataSource);
-            // get property from field name passed
-            System.Reflection.PropertyInfo propInfo = typeof(T).GetProperty(fieldName);
-            Comparison<Movie> compare = delegate (Movie a, Movie b)
+            if (!movies.Contains(movie))
             {
-                bool asc = sortDirection == SortDirection.Ascending;
-                object valueA = asc ? propInfo.GetValue(a, null) : propInfo.GetValue(b, null);
-                object valueB = asc ? propInfo.GetValue(b, null) : propInfo.GetValue(a, null);
-                //comparing the items
-                return valueA is IComparable ? ((IComparable)valueA).CompareTo(valueB) : 0;
-            };
-            returnList.Sort(compare);
-            return returnList;
+                foreach (var m in movies)
+                {
+                    if (movie.title == m.title)
+                        return;
+                }
+
+                movies.Add(movie);
+            }
+        }
+
+        public IEnumerable<Movie> all_movies_published_by_pixar()
+        {
+            var action_movies = new List<Movie>();
+            foreach (var m in all_movies())
+            {
+                if (m.production_studio == ProductionStudio.Pixar)
+                {
+                    action_movies.Add(m);
+                }
+            }
+            return action_movies;
+        }
+
+        public IEnumerable<Movie> all_movies_published_by_pixar_or_disney()
+        {
+            var action_movies = new List<Movie>();
+            foreach (var m in all_movies())
+            {
+                if (m.production_studio == ProductionStudio.Pixar
+                    || m.production_studio == ProductionStudio.Disney)
+                {
+                    action_movies.Add(m);
+                }
+            }
+            return action_movies;
+        }
+
+        public IEnumerable<Movie> all_movies_not_published_by_pixar()
+        {
+            var action_movies = new List<Movie>();
+            foreach (var m in all_movies())
+            {
+                if (m.production_studio != ProductionStudio.Pixar)
+                {
+                    action_movies.Add(m);
+                }
+            }
+            return action_movies;
+        }
+
+        public IEnumerable<Movie> all_movies_published_after(int year)
+        {
+            var action_movies = new List<Movie>();
+            foreach (var m in all_movies())
+            {
+                if (m.date_published.Year > year)
+                {
+                    action_movies.Add(m);
+                }
+            }
+            return action_movies;
+        }
+
+        public IEnumerable<Movie> all_movies_published_between_years(int startingYear, int endingYear)
+        {
+            var action_movies = new List<Movie>();
+            foreach (var m in all_movies())
+            {
+                if (m.date_published.Year >= startingYear
+                    && m.date_published.Year <= endingYear)
+                {
+                    action_movies.Add(m);
+                }
+            }
+            return action_movies;
+        }
+
+        public IEnumerable<Movie> all_kid_movies()
+        {
+            var action_movies = new List<Movie>();
+            foreach (var m in all_movies())
+            {
+                if (m.genre == Genre.kids)
+                {
+                    action_movies.Add(m);
+                }
+            }
+            return action_movies;
+        }
+
+        public IEnumerable<Movie> all_action_movies()
+        {
+            var action_movies = new List<Movie>();
+            foreach (var m in all_movies())
+            {
+                if (m.genre == Genre.action)
+                {
+                    action_movies.Add(m);
+                }
+            }
+            return action_movies;
         }
 
         public IEnumerable<Movie> sort_all_movies_by_title_descending()
         {
-           IEnumerable<Movie> stMovies = all_movies();
-            List<Movie> retMovies = CreateSortList<Movie>(stMovies, "title", SortDirection.Descending);
-            return retMovies;
-        }
-
-    public IEnumerable<Movie> sort_all_movies_by_title_ascending()
-    {
             IEnumerable<Movie> stMovies = all_movies();
-            List<Movie> retMovies = CreateSortList<Movie>(stMovies, "title", SortDirection.Ascending);
+            Movie m = new Movie();
+            List<Movie> retMovies = m.CreateSortList<Movie>(stMovies, "title", SortDirection.Descending);
             return retMovies;
         }
 
-    public IEnumerable<Movie> sort_all_movies_by_movie_studio_and_year_published()
-    {
-        throw new NotImplementedException();
+        public IEnumerable<Movie> sort_all_movies_by_title_ascending()
+        {
+            IEnumerable<Movie> stMovies = all_movies();
+            Movie m = new Movie();
+            List<Movie> retMovies = m.CreateSortList<Movie>(stMovies, "title", SortDirection.Ascending);
+            return retMovies;
+        }
+
+        public IEnumerable<Movie> sort_all_movies_by_movie_studio_and_year_published()
+        {
+            IEnumerable<Movie> stMovies = all_movies();
+            Movie m = new Movie();
+            List<Movie> retMovies = m.CreateSortList<Movie>(stMovies, "production_studio", SortDirection.Ascending,
+                "date_published", SortDirection.Ascending);
+            return retMovies;
+        }
+
+        public IEnumerable<Movie> sort_all_movies_by_date_published_descending()
+        {
+            IEnumerable<Movie> stMovies = all_movies();
+            Movie m = new Movie();
+            List<Movie> retMovies = m.CreateSortList<Movie>(stMovies, "date_published", SortDirection.Descending);
+            return retMovies;
+        }
+
+        public IEnumerable<Movie> sort_all_movies_by_date_published_ascending()
+        {
+            IEnumerable<Movie> stMovies = all_movies();
+            Movie m = new Movie();
+            List<Movie> retMovies = m.CreateSortList<Movie>(stMovies, "date_published", SortDirection.Ascending);
+            return retMovies;
+        }
     }
-
-    public IEnumerable<Movie> sort_all_movies_by_date_published_descending()
-    {
-            IEnumerable<Movie> stMovies = all_movies();
-            List<Movie> retMovies = CreateSortList<Movie>(stMovies, "date_published", SortDirection.Descending);
-            return retMovies;
-        }
-
-    public IEnumerable<Movie> sort_all_movies_by_date_published_ascending()
-    {
-            IEnumerable<Movie> stMovies = all_movies();
-            List<Movie> retMovies = CreateSortList<Movie>(stMovies, "date_published", SortDirection.Ascending);
-            return retMovies;
-        }
-  }
 }
