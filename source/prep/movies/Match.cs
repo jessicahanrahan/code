c95793e916a1fb658ef624a52768896eed902439
Jessica Hanrahan <jessica.hanrahan@tamu.edu>
2015-10-06 14:03:23 -0500

    Pushing new changes

--- source/prep/movies/Match.cs
+++ source/prep/movies/Match.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using code.matching;
+using code.prep.movies;
 
 namespace code.prep.movies
 {
+
+  public delegate ProductionStudio IGetAMoviesProductionStudio(Movie movie);
+
   public class Match<Item>
   {
-    public static void attribute(Func<object, object> func)
+    public static IGetAMoviesProductionStudio attribute(IGetAMoviesProductionStudio acc)
     {
-      throw new NotImplementedException();
+        return acc;
     }
   }
+
+
+    public static class AccessorExtensions
+    {
+    public static IMatchAn<Movie> equal_to(this IGetAMoviesProductionStudio accessor, ProductionStudio value)
+    {
+        return new AnonymousMatch<Movie>(x => x.production_studio == value);
+    }
+    }
 }
