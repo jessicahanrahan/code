69a9858da016fe7c730669da273ca38f608970e2
Jessica Hanrahan <jessica.hanrahan@tamu.edu>
2015-10-08 11:22:15 -0500

    Pushing new changes

--- source/web/WebHandlers.cs
+++ source/web/WebHandlers.cs
@@ -1,10 +1,28 @@
-﻿namespace code.web
+﻿using System;
+using System.Collections.Generic;
+
+namespace code.web
 {
   public class WebHandlers : IGetWebRequestHandlers
   {
-    public IHandleOneWebRequest get_handler_for_request(IProvideDetailsToHandlers request)
+      IEnumerable<IHandleOneWebRequest> all_handlers;
+
+      public WebHandlers(IEnumerable<IHandleOneWebRequest> all_handlers)
+      {
+          this.all_handlers = all_handlers;
+      }
+
+      public IHandleOneWebRequest get_handler_for_request(IProvideDetailsToHandlers request)
     {
-      throw new System.NotImplementedException();
+        foreach (var handler in all_handlers)
+        {
+            if (handler.can_process(request))
+            {
+                return handler;
+            }
+        }
+
+        throw new NotImplementedException("Either at least one handler needs to be matched or null case needs to be handled");
     }
   }
 }
